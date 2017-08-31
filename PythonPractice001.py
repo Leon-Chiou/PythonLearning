7ae6af310a4789d38a4bb1a8033751edcb8902ff
Leon <leon6231023@outlook.com>
2017-08-31 17:43:32 +0800

    12

--- PythonPractice001.py
+++ PythonPractice001.py
@@ -3,3 +3,4 @@ import nester
 cast = ["Leon", "Jennie", "Chloe", "Ann", "Natasha", ["Leon", "Jennie", ["Leon", "Jennie", "Chloe", "Ann", "Natasha"], "Chloe", "Ann", "Natasha"]]
 
 nester.nestPrint(cast)
+print('test')
