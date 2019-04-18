5b51a3b8b0ea40963a7a99e8cc4207e45e139bfd
Leon <31308914+Leon-Chiou@users.noreply.github.com>
2019-04-18 14:04:05 +0800

    first try

---
+++ revitDesignscriptTest.py
@@ -0,0 +1,6 @@
+from Autodesk.Revit.UI import Events
+Events.ApplicationClosingEventArgs()
+
+from Autodesk.DesignScript.Geometry import Geometry
+from Autodesk.Revit.UI.Selection import Selection
+Selection.GetElementIds()
