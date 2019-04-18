a73305c2cbed9d3fdbeb8f92e6119ebb2f39fe0d
Leon <31308914+Leon-Chiou@users.noreply.github.com>
2019-04-18 14:07:19 +0800

    second try

--- revitDesignscriptTest.py
+++ revitDesignscriptTest.py
@@ -4,3 +4,6 @@ Events.ApplicationClosingEventArgs()
 from Autodesk.DesignScript.Geometry import Geometry
 from Autodesk.Revit.UI.Selection import Selection
 Selection.GetElementIds()
+
+from Autodesk.Revit.UI import ExternalCommandData
+ExternalCommandData.GetElementIds()
